8a72d5a5a3a84a43ed8c94b0ead9f3e2a51d56c9
Hayden Shelton <hayden.shelton@utah.edu>
2017-07-16 18:29:11 -0600

    server side rsvp model

---
+++ modules/rsvp/server/models/rsvp.model.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * User Schema
+ */
+var RsvpSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  name: {
+  	type: String,
+    required: true
+  },
+  attending: {
+    type: boolean,
+    default: true
+  },
+  note: {
+    type: string
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+);
+
+mongoose.model('Rsvp', ArticleSchema);
