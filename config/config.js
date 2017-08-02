e7bbdf7055dc9a80b7361adecb628f4420823bdd
dylan-wright <dylan.wright@squarehook.com>
2017-08-02 14:16:26 -0600

    changed file size limit

--- config/config.js
+++ config/config.js
@@ -39,7 +39,8 @@ module.exports = {
       bucket: process.env.TOOMEAN_AWS_S3_BUCKET,
       acl: 'public-read',
       limits: {
-        fileSize: 1*1024*1024
+        // 100M
+        fileSize: 100*1024*1024
       }
     },
     profilePicture: {
