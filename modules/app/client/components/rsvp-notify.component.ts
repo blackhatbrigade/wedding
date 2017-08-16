a1fd89693c56b470935a5c359890f2e47624abf9
Hayden Shelton <hayden.shelton@utah.edu>
2017-08-15 21:24:24 -0600

    added rsvp notification

---
+++ modules/app/client/components/rsvp-notify.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../auth/client/services/auth.service';
+import { RsvpService } from '../../../rsvp/client/services/rsvp.service';
+import { Subscription } from 'rxjs/Subscription';
+
+@Component({
+  templateUrl: './../views/rsvp-notify.html',
+  selector: 'rsvp-notify'
+})
+export class RsvpNotifyComponent implements OnInit{
+  needsRsvp: boolean = false;
+
+
+  constructor(
+    private AuthService :AuthService,
+    private RsvpService: RsvpService
+  ){
+    
+  }
+
+  ngOnInit(){
+    if(this.AuthService.loggedIn){
+      this.checkRsvp();
+    }
+    
+    //notify if necessary
+    this.AuthService.authChanged$.subscribe(
+    loggedIn => {
+      if(loggedIn){
+        this.checkRsvp();
+      }
+    });
+  }
+
+  checkRsvp(){
+    this.RsvpService.getUserRsvp()
+    .subscribe(rsvp => {
+      this.needsRsvp = !rsvp.name;
+    });
+  }
+
+
+
+}
